ac5f47a13f2682f5a2ca8128e46611316cec344e
henryflanagan <henrflan986@ed.amdsb.ca>
2025-04-03 14:33:23 -0400

    Add project files.

---
+++ Football/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Football
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.optionButton5 = new System.Windows.Forms.Button();
+            this.optionButton2 = new System.Windows.Forms.Button();
+            this.optionButton4 = new System.Windows.Forms.Button();
+            this.optionLabel3 = new System.Windows.Forms.Label();
+            this.optionLabel4 = new System.Windows.Forms.Label();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.option2Output = new System.Windows.Forms.Label();
+            this.option3Output = new System.Windows.Forms.Label();
+            this.option4Output = new System.Windows.Forms.Label();
+            this.storyImageBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.storyImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // optionButton5
+            // 
+            this.optionButton5.Location = new System.Drawing.Point(57, 152);
+            this.optionButton5.Name = "optionButton5";
+            this.optionButton5.Size = new System.Drawing.Size(94, 25);
+            this.optionButton5.TabIndex = 0;
+            this.optionButton5.UseVisualStyleBackColor = true;
+            this.optionButton5.Click += new System.EventHandler(this.option5Button_Click);
+            // 
+            // optionButton2
+            // 
+            this.optionButton2.Location = new System.Drawing.Point(63, 206);
+            this.optionButton2.Name = "optionButton2";
+            this.optionButton2.Size = new System.Drawing.Size(87, 29);
+            this.optionButton2.TabIndex = 1;
+            this.optionButton2.UseVisualStyleBackColor = true;
+            this.optionButton2.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // optionButton4
+            // 
+            this.optionButton4.Location = new System.Drawing.Point(56, 263);
+            this.optionButton4.Name = "optionButton4";
+            this.optionButton4.Size = new System.Drawing.Size(116, 24);
+            this.optionButton4.TabIndex = 2;
+            this.optionButton4.UseVisualStyleBackColor = true;
+            this.optionButton4.Click += new System.EventHandler(this.optionButton4_Click);
+            // 
+            // optionLabel3
+            // 
+            this.optionLabel3.AutoSize = true;
+            this.optionLabel3.Location = new System.Drawing.Point(197, 216);
+            this.optionLabel3.Name = "optionLabel3";
+            this.optionLabel3.Size = new System.Drawing.Size(0, 16);
+            this.optionLabel3.TabIndex = 4;
+            // 
+            // optionLabel4
+            // 
+            this.optionLabel4.AutoSize = true;
+            this.optionLabel4.Location = new System.Drawing.Point(209, 272);
+            this.optionLabel4.Name = "optionLabel4";
+            this.optionLabel4.Size = new System.Drawing.Size(0, 16);
+            this.optionLabel4.TabIndex = 5;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Location = new System.Drawing.Point(343, 118);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(222, 218);
+            this.outputLabel.TabIndex = 6;
+            this.outputLabel.Text = "                                                              ";
+            // 
+            // option2Output
+            // 
+            this.option2Output.AutoSize = true;
+            this.option2Output.Location = new System.Drawing.Point(244, 160);
+            this.option2Output.Name = "option2Output";
+            this.option2Output.Size = new System.Drawing.Size(44, 16);
+            this.option2Output.TabIndex = 7;
+            this.option2Output.Text = "label1";
+            // 
+            // option3Output
+            // 
+            this.option3Output.AutoSize = true;
+            this.option3Output.Location = new System.Drawing.Point(241, 219);
+            this.option3Output.Name = "option3Output";
+            this.option3Output.Size = new System.Drawing.Size(44, 16);
+            this.option3Output.TabIndex = 8;
+            this.option3Output.Text = "label1";
+            // 
+            // option4Output
+            // 
+            this.option4Output.AutoSize = true;
+            this.option4Output.Location = new System.Drawing.Point(238, 277);
+            this.option4Output.Name = "option4Output";
+            this.option4Output.Size = new System.Drawing.Size(0, 16);
+            this.option4Output.TabIndex = 9;
+            // 
+            // storyImageBox
+            // 
+            this.storyImageBox.Location = new System.Drawing.Point(56, 12);
+            this.storyImageBox.Name = "storyImageBox";
+            this.storyImageBox.Size = new System.Drawing.Size(288, 103);
+            this.storyImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.storyImageBox.TabIndex = 10;
+            this.storyImageBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(564, 345);
+            this.Controls.Add(this.storyImageBox);
+            this.Controls.Add(this.option4Output);
+            this.Controls.Add(this.option3Output);
+            this.Controls.Add(this.option2Output);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.optionLabel4);
+            this.Controls.Add(this.optionLabel3);
+            this.Controls.Add(this.optionButton4);
+            this.Controls.Add(this.optionButton2);
+            this.Controls.Add(this.optionButton5);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.storyImageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Button optionButton3;
+        private System.Windows.Forms.Button option4Button;
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.Label option2Label;
+        private System.Windows.Forms.Label option3Label;
+        private System.Windows.Forms.Label option4Label;
+        private System.Windows.Forms.Button optionButton1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button optionButton5;
+        private System.Windows.Forms.Button optionButton2;
+        private System.Windows.Forms.Button optionButton4;
+        private System.Windows.Forms.Label optionLabel3;
+        private System.Windows.Forms.Label optionLabel4;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label option2Output;
+        private System.Windows.Forms.Label option3Output;
+        private System.Windows.Forms.Label option4Output;
+        private System.Windows.Forms.PictureBox storyImageBox;
+    }
+}
+
---
+++ Football/Form1.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Football
+{
+    public partial class Form1 : Form
+    {
+        int page = 1;
+        Random randGen = new Random();
+        SoundPlayer sPlayer = new SoundPlayer();
+        public Form1()
+        {
+            InitializeComponent();
+            DisplayPage();
+        }
+
+        private void option5Button_Click(object sender, EventArgs e)
+        {
+          
+
+            if (page == 1)
+            {
+                int chance = randGen.Next(1, 101);
+
+                if (chance > 50)
+                {
+                    page = 3;
+                }
+                else
+                {
+                    page = 2;
+                }
+            }
+            else if (page == 5)
+            {
+                page = 6;
+            }
+           
+            else if (page == 6)
+            {
+                page = 8;
+            }
+            else if (page == 7)
+            {
+                page = 11;
+            }
+            else if (page == 9)
+            {
+                page = 13;
+            }
+            else if (page == 11)
+            {
+                page = 15;
+            }
+            else if (page == 12)
+            {
+                page = 15;
+            }
+            else if (page == 13)
+            {
+                page = 3;
+            }
+            else if (page == 14)
+            {
+                page = 3;
+            }
+            else if (page == 15)
+            {
+                page = 3;
+            }
+            DisplayPage();
+        }
+
+        private void DisplayPage()
+        {
+            /// Display text and game options to screen based on the 
+            /// current page
+
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = "Start drive";
+                    option2Output.Text = "Run";
+                    option3Output.Text = "Pass";
+                   sPlayer = new SoundPlayer(Properties.Resources.startUpSound);
+                   sPlayer.Play();
+                    storyImageBox.Image = Properties.Resources.football_field;
+                    break;
+                case 2:
+                    outputLabel.Text = "Your Qb threw an interception, play again?";
+                    option2Output.Text = "Yes";
+                    option3Output.Text = "No";
+                   sPlayer = new SoundPlayer(Properties.Resources.lostSound);
+                    sPlayer.Play();
+                    storyImageBox.Image = Properties.Resources.qbInterseption;
+                    break;
+                case 3:
+                    outputLabel.Text = "You got a touchdown, you win, play again?";
+                    option2Output.Text = "Yes";
+                    option3Output.Text = "No";
+                    sPlayer = new SoundPlayer(Properties.Resources.winningSound);
+                    sPlayer.Play();
+                    storyImageBox.Image = Properties.Resources.touchdownScore;
+                    break;
+                case 4:
+                    outputLabel.Text = "Your running back fumbled, play again?";
+                   option2Output.Text = "Yes";
+                  option3Output.Text = "No";
+                   sPlayer = new SoundPlayer(Properties.Resources.lostSound);
+                  sPlayer.Play();
+                    storyImageBox.Image = Properties.Resources.rbFumble;
+                    break;
+                case 5:
+                    outputLabel.Text = "You gained 5 yards, would you like to pass or rush?";
+                    option2Output.Text = "Pass";
+                   option3Output.Text = "Rush";
+                    break;
+                case 6:
+                    outputLabel.Text = "You are going for a passing play, would you like to go for a deep pass, short pass or screen pass?";
+                   option2Output.Text = "Deep pass";
+                  option3Output.Text = "short pass";
+                   option4Output.Text = "screen pass";
+                    break;
+                case 7:
+                    outputLabel.Text = "You are going for a rushing play, would you like to go for an imside or outside rush?";
+                    option2Output.Text = "Inside run";
+                    option4Output.Text = "outside run";
+                    break;
+                case 8:
+                    outputLabel.Text = "You are going for a deep pass play, would you like to thrwo to Wr1, Wr2, Wr3?";
+                    option2Output.Text = "Wr1";
+                   option3Output.Text = "Wr2";
+                    option4Output.Text = "Wr3";
+                    break;
+                case 9:
+                    outputLabel.Text = "You are going for a screen pass, would you like to throw to Wr1. Wr2, or Rb?";
+                    option2Output.Text = "Wr1";
+                   option3Output.Text = "Wr2";
+                    option4Output.Text = "Rb";
+                    break;
+                case 10:
+                    outputLabel.Text = "You are going for a short pass, would you like to throw to Wr1. Wr2, or Te?";
+                    option2Output.Text = "Wr1";
+                   option3Output.Text = "Wr2";
+                    option4Output.Text = "Te";
+                    break;
+                case 11:
+                    outputLabel.Text = "You are going for a outside rush, would you like to rush with Rb1, Rb2, or Qb?";
+                    option2Output.Text = "Wr1";
+                   option3Output.Text = "Wr2";
+                    option4Output.Text = "Qb";
+                    break;
+                case 12:
+                    outputLabel.Text = "You are going for a inside rush, would you like to rush with Rb1, Rb2, or Qb?";
+                    option2Output.Text = "Wr1";
+                   option3Output.Text = "Wr2";
+                    option4Output.Text = "Qb";
+                    break;
+                case 13:
+                    outputLabel.Text = "You gained 20 yards, would you like to pass or rush?";
+                    option2Output.Text = "Rush";
+                   option3Output.Text = "Pass";
+                    break;
+                case 14:
+                    outputLabel.Text = "You gained 10 yard would you like to pass or rush?";
+                    option2Output.Text = "rush";
+                   option3Output.Text = "pass";
+                    break;
+                case 15:
+                    outputLabel.Text = "gain 5 yards, would you like to pass or rush?";
+                    option2Output.Text = "Rush";
+                   option3Output.Text = "Pass";
+                    break;
+
+
+            }
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            if (page == 1)
+            {
+                page = 5;
+            }
+            else if (page == 5)
+            {
+                page = 7;
+            }
+
+            else if (page == 6)
+            {
+                page = 9;
+            }
+            else if (page == 9)
+            {
+                page = 2;
+            }
+            else if (page == 10)
+            {
+                page = 3;
+            }
+            else if (page == 8)
+            {
+                page = 3;
+            }
+            else if (page == 13)
+            {
+                page = 2;
+            }
+            else if (page == 14)
+            {
+                page = 4;
+            }
+            else if (page == 11)
+            {
+                page = 4;
+            }
+            else if (page == 12)
+            {
+                page = 3;
+            }
+            else if (page == 15)
+            {
+                page = 4;
+            }
+            DisplayPage();
+        }
+
+        private void optionButton4_Click(object sender, EventArgs e)
+        {
+            if (page == 6)
+            {
+                page = 10;
+            }
+            else if (page == 8)
+            {
+                page = 13;
+            }
+            else if (page == 9)
+            {
+                page = 2;
+            }
+            else if (page == 10)
+            {
+                page = 14;
+            }
+            else if (page == 11)
+            {
+                page = 3;
+            }
+            else if (page == 10)
+            {
+                page = 4;
+            }
+            DisplayPage();
+        }
+
+      
+    }
+}
